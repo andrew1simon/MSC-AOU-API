cc12b2307c3691802b3d2526e741da8c663e878c
Ahmed Hassan <ahmed.hassan52852@gmail.com>
2023-11-23 16:41:20 +0200

    V1.0

--- controllers/news.js
+++ controllers/news.js
@@ -5,12 +5,16 @@ async function GetAllNews(req,res) {
     let allNews = await prisma.news.findMany();
     res.send(allNews)
 }
-async function CreateNewNews(req,res) {
-    let {title,content} = req.body
-    await prisma.news.create({data: {title , content}})
-    res.send("Done")
+async function CreateNewNews(req, res) {
+    let { title, content, hasImg, subTitle } = req.body;
+    try {
+        await prisma.news.create({ data: { title, content, hasImg, subTitle} });
+        res.status(201).json({ msg: 'News Created' })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Server Error' })
+    }
 }
 module.exports = { 
 	GetAllNews , CreateNewNews
 }
-
